9ed830f5ff71bce3d9d467158dfd3f5104d0fde8
Ade Adeola <adedotster@gmail.com>
2025-01-26 21:44:02 +0000

    chore: add shallow compare example

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,12 +1,14 @@
 import { View, StyleSheet } from 'react-native';
 import CustomCompareEffect from './routes/custom-compare-effect';
 import DeepCompareEffect from './routes/deep-compare-effect';
+import ShallowCompareEffect from './routes/shallow-compare-effect';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <CustomCompareEffect />
       <DeepCompareEffect />
+      <ShallowCompareEffect />
     </View>
   );
 }
